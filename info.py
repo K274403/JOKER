a1fe8dde726e652f7981b1188c8a16719c008431
Krishna Prajapati <158284550+K274403@users.noreply.github.com>
2025-05-02 13:45:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002396546048') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Deepak:Deepak@cluster0.gq9gf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rohan:Rohan@cluster0.jajgrik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Deepak")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deepak_files')
 
