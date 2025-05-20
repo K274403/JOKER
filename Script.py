82060b2b4a867f1f5ecd6e06618a272f286ea5c6
Krishna Prajapati <158284550+K274403@users.noreply.github.com>
2025-05-20 14:26:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -311,11 +311,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b>[@RKMOVIESZIP]<a href="https://t.me/Rkmovieszip">{filename}</a> 
-
-<blockquote>✅ 𝘼𝙡𝙨𝙤 𝙟𝙤𝙞𝙣 #𝙍𝙠𝙢𝙤𝙫𝙞𝙚𝙯𝙞𝙥 – #𝙉𝙤𝟏 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙁𝙧𝙚𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗 𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡. 
-
-⏤‌‌𝗝⌡𝗼𝗶𝗻 𝗣𝗥𝗘𝗠𝗜𝗨𝗠 –「 @RKMOVIESZIP 」🔥</blockquote></b>"""
+    CAPTION = """<b>[@RKMOVIESZIP]<a href="https://t.me/Rkmovieszip">{filename}</a><blockquote>✅ 𝘼𝙡𝙨𝙤 𝙟𝙤𝙞𝙣 #𝙍𝙠𝙢𝙤𝙫𝙞𝙚𝙯𝙞𝙥 – #𝙉𝙤𝟏 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙁𝙧𝙚𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗 𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡.⏤‌‌𝗝⌡𝗼𝗶𝗻 𝗣𝗥𝗘𝗠𝗜𝗨𝗠 –「 @RKMOVIESZIP 」🔥</blockquote></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
