bd723d98fe4d5d8d8cfd204b1ec50ef2f1e34644
Krishna Prajapati <158284550+K274403@users.noreply.github.com>
2025-06-23 20:23:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/rkmoviezip'
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002023191399'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002023191399'))
 
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
